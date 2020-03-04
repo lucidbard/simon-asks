d77baa9c1ac6f98f7777acb39be44e51c3977e82
Seebright <john@seebright.com>
2020-03-04 12:14:43 -0500

    Updates

---
+++ precache-manifest.0d12547a97e34e96f7df5f8706c8f302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b68764d383f08c05300a729d7422308",
+    "url": "/simon-asks/index.html"
+  },
+  {
+    "revision": "4304c5258630836b6f0d",
+    "url": "/simon-asks/static/css/main.32876263.chunk.css"
+  },
+  {
+    "revision": "969986512ba9fd4044fe",
+    "url": "/simon-asks/static/js/2.d8f30ed0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/simon-asks/static/js/2.d8f30ed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4304c5258630836b6f0d",
+    "url": "/simon-asks/static/js/main.c83fe6a0.chunk.js"
+  },
+  {
+    "revision": "f889afbc3b2528ac3a3b",
+    "url": "/simon-asks/static/js/runtime-main.75d6299d.js"
+  }
+]);
